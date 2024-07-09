d4b67a0c8a415248a5c68d01a372ff2afb668af8
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-07-09 15:28:39 +0530

    update: 404 error file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+	return (
+		<div className="bg-black-100  flex flex-col justify-center items-center h-screen">
+			<div className="mt-8">
+				<Image src={"/404.svg"} alt="404Error" width={600} height={600} />
+			</div>
+			<div className="text-xl">
+				Go back to{" "}
+				<Link href="/" className=" text-purple  font-semibold">
+					Home
+				</Link>
+			</div>
+		</div>
+	);
+}
