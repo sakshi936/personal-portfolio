d529b46fabba98060c0f875c597505edbb34ea91
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-07-09 17:51:57 +0530

    fix: about font and scroll effect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,14 +15,14 @@ function About() {
 			<div className="flex ">
 				<Image src="/coding2.svg" alt="me" width={500} height={500} className="hidden md:block" />
 				<div className="px-4 md:px-10 py-20  md:py-28">
-					<h1 className="text-xl font-medium ">I&apos;m Sakshi&#44;</h1>
+					<h1 className="text-xl font-semibold ">I&apos;m Sakshi&#44;</h1>
 					<p className="text-slate-400 mt-3">
 						{" "}
 						A dedicated and passionate developer on a journey to master Full Stack Web Development. With a solid foundation in Computer Applications, I&apos;m continuously expanding
 						my skill set to include both frontend and backend technologies.My focus is on designing and developing websites and applications that integrate functionality
 						effortlessly.
 					</p>
-					<h2 className="mt-5 text-xl font-medium text-purple mb-5">Working to be better than yesterday!</h2>
+					<h2 className="mt-5 text-lg font-medium text-purple mb-5">Working to be better than yesterday!</h2>
 
 					<div className="w-fit">
 						<Link href="#project">
